bb95b51fd14caad2d671a1591041a6c9da9ed603
Ritik khandelwal <Khandelwalritik08@gmail.com>
2024-08-02 12:13:49 +0530

    Updated the design and added some features like calculate total time taken, average time etc

--- functions/home.js
+++ functions/home.js
@@ -1,20 +1,31 @@
-let typesofcalculation = ["add", "sub", "mul", "div"];
-let total_number_of_questions = 5;
+let typesofcalculation = ["add"];
+let total_number_of_questions = 2;
 let questionsAnswered = 0;
 let ending_range = 10;
 const delay = 300;
+let total_time = 0;
+let average_time = 0;
+let startTime;
 
 function startQuiz() {
     if (questionsAnswered < total_number_of_questions) {
         generateQuestion();
     } else {
-        console.log("Quiz completed!");
+        console.log("Quiz completed! Total Time: " + (total_time / 1000) + " seconds");
+        let total_time_field = document.querySelector(".total_time");
+        total_time_field.innerHTML = `Total Time: ${(total_time / 1000).toFixed(2)} sec`;
+
+        average_time = total_time / total_number_of_questions;
+
+        let average_time_field = document.querySelector(".average_time");
+        average_time_field.innerHTML = `Average Time: ${(average_time / 1000).toFixed(2)} sec`;
     }
 }
 
 // function to generate questions
 
 function generateQuestion() {
+    startTime = performance.now(); // Start the timer when a question is generated
     let randomIndex = Math.floor(Math.random() * typesofcalculation.length);
     let currentCalculation = typesofcalculation[randomIndex];
 
@@ -39,10 +50,16 @@ function add() {
     user_answer.style.color = "";
 
     const listener = () => {
-        if (user_answer.value == actual_sum) {
+        if (parseInt(user_answer.value) == actual_sum) {
             user_answer.style.color = "#4C7F4D";
             questionsAnswered++;
             console.log("Correct answer! Questions answered: " + questionsAnswered);
+
+            let endTime = performance.now();
+            let time_taken = endTime - startTime;
+            console.log("Time Taken: " + time_taken);
+            total_time += time_taken;
+
             user_answer.removeEventListener('input', listener);
             setTimeout(startQuiz, delay); // Call startQuiz to generate the next question
         }
@@ -66,10 +83,16 @@ function sub() {
     user_answer.style.color = "";
 
     const listener = () => {
-        if (user_answer.value == actual_sub) {
+        if (parseInt(user_answer.value) == actual_sub) {
             user_answer.style.color = "#4C7F4D";
             questionsAnswered++;
             console.log("Correct answer! Questions answered: " + questionsAnswered);
+
+            let endTime = performance.now();
+            let time_taken = endTime - startTime;
+            console.log("Time Taken: " + time_taken);
+            total_time += time_taken;
+
             user_answer.removeEventListener('input', listener);
             setTimeout(startQuiz, delay); // Call startQuiz to generate the next question
         }
@@ -93,10 +116,16 @@ function mul() {
     user_answer.style.color = "";
 
     const listener = () => {
-        if (user_answer.value == actual_mul) {
+        if (parseInt(user_answer.value) == actual_mul) {
             user_answer.style.color = "#4C7F4D";
             questionsAnswered++;
             console.log("Correct answer! Questions answered: " + questionsAnswered);
+
+            let endTime = performance.now();
+            let time_taken = endTime - startTime;
+            console.log("Time Taken: " + time_taken);
+            total_time += time_taken;
+
             user_answer.removeEventListener('input', listener);
             setTimeout(startQuiz, delay); // Call startQuiz to generate the next question
         }
@@ -124,6 +153,12 @@ function div() {
             user_answer.style.color = "#4C7F4D";
             questionsAnswered++;
             console.log("Correct answer! Questions answered: " + questionsAnswered);
+
+            let endTime = performance.now();
+            let time_taken = endTime - startTime;
+            console.log("Time Taken: " + time_taken);
+            total_time += time_taken;
+
             user_answer.removeEventListener('input', listener);
             setTimeout(startQuiz, delay); // Call startQuiz to generate the next question
         }
